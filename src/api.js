e296a242f1fe2bef82a7e9970efbacf5b4614f4f
José Carlos <130189368+Dev-Josee@users.noreply.github.com>
2025-08-28 20:54:01 -0300

    corrigido as rotas da api

--- src/api.js
+++ src/api.js
@@ -15,14 +15,14 @@ export const getBioHistory = () => api.get('/bio/history');
 // Fotos
 
 export const uploadPhoto = (formData) => {
-   return api.post('/api/photos/uploads', formData); // Axios  Content-Type automaticamente
+   return api.post('/photos/uploads', formData); // Axios  Content-Type automaticamente
 };
 export const getPhotos = (page = 1, limit = 10) => api.get(`/photos/?page=${page}&limit=${limit}`);
 
 // função é async e usa o api
 export const getPhotosByCategory = async (category, page = 1, limit = 10) => {
     try {
-        const response = await api.get(`/api/photos/${category}?page=${page}&limit=${limit}`);
+        const response = await api.get(`/photos/${category}?page=${page}&limit=${limit}`);
         console.log('Resposta da API:', response.data)
         return response.data;
     } catch (error) {
